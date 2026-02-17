b6c8a373c23af851175edd3b5e4d13e05b756464
RezaPaseban1991 <rezapaseban70@yahoo.com>
2026-02-16 23:40:48 -0800

    Update Bmi.py

--- Bmi.py
+++ Bmi.py
@@ -1,23 +1,48 @@
-a = float (input(' how many KG is your weight:'))
-b = float (input('how many meter is your hight:'))
-bmi = a / b**2
+#this program calculate BMI 
+#input
+weight = int(input( 'If you know your weight in pound enter 1, if you know it in KG enter 2: '))
+
+if weight == 1:
+    
+    weight_p = float (input(' How many pound is your weight:'))
+    main_weight = weight_p * 0.453592
+else:
+    weight_k = float (input(' How many KG is your weight:'))
+    main_weight = weight_k
+    
+
+hight = int(input( 'If you know your hight in foot enter 1, if you konw it in meter enter 2: '))
+
+if hight == 1:
+    hight_f = float (input('How many foot is your hight:'))
+    main_hight = hight_f * 0.3048
+    
+else:
+    hight_m = float (input('How many meter is your hight:'))
+    main_hight = hight_m
+
+#cal   
+bmi = main_weight / main_hight ** 2
 if bmi < 15:
-    d = 'very severely under weight'
+    result = 'very severely under weight'
 elif bmi >= 15 and bmi < 16:
-    d = 'severely under weight'
+    result = 'severely under weight'
 elif bmi >= 16 and bmi < 18.5:
-    d =  'under weight'
+    result =  'under weight'
 elif bmi >= 18.5 and bmi < 25:
-    d = 'normal'
+    result = 'normal'
 elif bmi >= 25 and bmi < 30: 
-    d = 'over weight'
+    result = 'over weight'
 elif bmi >= 30 and bmi < 35:
-    d = 'moderteky obese'
+    result = 'moderteky obese'
 elif bmi >= 35 and bmi < 40:
-    d = 'severely obese'
+    result = 'severely obese'
 elif bmi >= 40:
-    d = 'very severely obese'
-x = round (bmi, 2)
-print('Your BMI is ' + str(x) + ' so your are in ' + d + ' level.' )
+    result = 'very severely obese'
+bmi_round = format(bmi, '.2f')
+
+#output
+print('Your BMI is ',bmi_round,' so your are in ' + result + ' level.' )
+
     
     
